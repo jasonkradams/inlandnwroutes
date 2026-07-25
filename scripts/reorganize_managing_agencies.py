43d1a770b605aedc53db8f0785720fe092eab666
Jason Adams <jason.k.r.adams@gmail.com>
2026-07-24 21:57:29 -0700

    feat(resources): add clickable tel: links for all phone numbers in Managing Agencies directory

--- scripts/reorganize_managing_agencies.py
+++ scripts/reorganize_managing_agencies.py
@@ -62,27 +62,27 @@ conditions, trail washouts, and active wildfire closures.
 
 | National Forest / District | Phone | Website & Alerts Link |
 | :--- | :--- | :--- |
-| **Idaho Panhandle Forests** | 208-765-7223 | [IPNF Alerts](https://www.fs.usda.gov/alerts/ipnf/alerts-notices) |
-| Bonners Ferry District | 208-267-5561 | [Bonners Ferry](https://www.fs.usda.gov/ipnf) |
-| Priest Lake District | 208-443-2512 | [Priest Lake](https://www.fs.usda.gov/ipnf) |
-| Sandpoint District | 208-263-5111 | [Sandpoint](https://www.fs.usda.gov/ipnf) |
-| CDA River District | 208-769-3000 | [CDA River](https://www.fs.usda.gov/ipnf) |
-| St. Joe District (Avery) | 208-245-4517 | [St. Joe](https://www.fs.usda.gov/ipnf) |
-| St. Joe District (St. Maries) | 208-245-2531 | [St. Joe](https://www.fs.usda.gov/ipnf) |
-| **Kootenai Forest** | 406-293-6211 | [Kootenai Alerts](https://www.fs.usda.gov/alerts/kootenai/alerts-notices) |
-| Libby / Fisher River District | 406-293-8861 | [Libby District](https://www.fs.usda.gov/kootenai) |
-| Three Rivers District | 406-295-4693 | [Three Rivers](https://www.fs.usda.gov/kootenai) |
-| Rexford District | 406-296-2536 | [Rexford District](https://www.fs.usda.gov/kootenai) |
-| Cabinet District | 406-827-3533 | [Cabinet District](https://www.fs.usda.gov/kootenai) |
-| **Colville Forest** | 509-684-7000 | [Colville Alerts](https://www.fs.usda.gov/alerts/colville/alerts-notices) |
-| Colville / Kettle Falls District | 509-684-3711 | [Colville District](https://www.fs.usda.gov/colville) |
-| Republic District | 509-775-3305 | [Republic District](https://www.fs.usda.gov/colville) |
-| Sullivan Lake District | 509-446-7500 | [Sullivan Lake](https://www.fs.usda.gov/colville) |
-| Newport District | 509-447-3129 | [Newport District](https://www.fs.usda.gov/colville) |
-| **Lolo Forest** | 406-329-3750 | [Lolo Alerts](https://www.fs.usda.gov/alerts/lolo/alerts-notices) |
-| Superior District | 406-822-4233 | [Superior District](https://www.fs.usda.gov/lolo) |
-| **Nez Perce-Clearwater** | 208-983-1950 | [Alerts](https://www.fs.usda.gov/alerts/nezperceclearwater/alerts-notices) |
-| **Flathead Forest** | 406-758-5208 | [Flathead Alerts](https://www.fs.usda.gov/alerts/flathead/alerts-notices) |
+| **IPNF Main Office** | [208-765-7223](tel:2087657223) | [IPNF Alerts](https://www.fs.usda.gov/ipnf) |
+| Bonners Ferry District | [208-267-5561](tel:2082675561) | [Bonners Ferry](https://www.fs.usda.gov/ipnf) |
+| Priest Lake District | [208-443-2512](tel:2084432512) | [Priest Lake](https://www.fs.usda.gov/ipnf) |
+| Sandpoint District | [208-263-5111](tel:2082635111) | [Sandpoint](https://www.fs.usda.gov/ipnf) |
+| CDA River District | [208-769-3000](tel:2087693000) | [CDA River](https://www.fs.usda.gov/ipnf) |
+| St. Joe District (Avery) | [208-245-4517](tel:2082454517) | [St. Joe](https://www.fs.usda.gov/ipnf) |
+| St. Joe District (St. Maries) | [208-245-2531](tel:2082452531) | [St. Joe](https://www.fs.usda.gov/ipnf) |
+| **Kootenai Main Office** | [406-293-6211](tel:4062936211) | [Kootenai Alerts](https://www.fs.usda.gov/kootenai) |
+| Libby / Fisher River | [406-293-8861](tel:4062938861) | [Libby District](https://www.fs.usda.gov/kootenai) |
+| Three Rivers District | [406-295-4693](tel:4062954693) | [Three Rivers](https://www.fs.usda.gov/kootenai) |
+| Rexford District | [406-296-2536](tel:4062962536) | [Rexford District](https://www.fs.usda.gov/kootenai) |
+| Cabinet District | [406-827-3533](tel:4068273533) | [Cabinet District](https://www.fs.usda.gov/kootenai) |
+| **Colville Main Office** | [509-684-7000](tel:5096847000) | [Colville Alerts](https://www.fs.usda.gov/colville) |
+| Colville / Kettle Falls | [509-684-3711](tel:5096843711) | [Colville District](https://www.fs.usda.gov/colville) |
+| Republic District | [509-775-3305](tel:5097753305) | [Republic District](https://www.fs.usda.gov/colville) |
+| Sullivan Lake District | [509-446-7500](tel:5094467500) | [Sullivan Lake](https://www.fs.usda.gov/colville) |
+| Newport District | [509-447-3129](tel:5094473129) | [Newport District](https://www.fs.usda.gov/colville) |
+| **Lolo Main Office** | [406-329-3750](tel:4063293750) | [Lolo Alerts](https://www.fs.usda.gov/lolo) |
+| Superior District | [406-822-4233](tel:4068224233) | [Superior District](https://www.fs.usda.gov/lolo) |
+| **Nez Perce-Clearwater** | [208-983-1950](tel:2089831950) | [Alerts](https://www.fs.usda.gov/nezperceclearwater) |
+| **Flathead Forest** | [406-758-5208](tel:4067585208) | [Flathead Alerts](https://www.fs.usda.gov/flathead) |
 
 ---
 
@@ -90,28 +90,28 @@ conditions, trail washouts, and active wildfire closures.
 
 | Managing Agency / Park | Phone | Website Link |
 | :--- | :--- | :--- |
-| **Idaho Dept. of Parks & Rec.** | 208-769-1511 | [idahoparks.org](https://idahoparks.org) |
-| Farragut State Park | 208-683-2425 | [Farragut](https://idahoparks.org/parks/farragut) |
-| Heyburn State Park | 208-686-1308 | [Heyburn](https://idahoparks.org/parks/heyburn) |
-| Old Mission State Park | 208-682-3814 | [Old Mission](https://idahoparks.org/parks/coeur-d-alenes-old-mission) |
-| Priest Lake State Park | 208-443-2200 | [Priest Lake](https://idahoparks.org/parks/priest-lake) |
-| Round Lake State Park | 208-263-3489 | [Round Lake](https://idahoparks.org/parks/round-lake) |
-| **Washington State Parks** | 509-902-8500 | [parks.wa.gov](https://parks.wa.gov) |
-| Mount Spokane State Park | 509-456-3964 | [Mount Spokane](https://parks.wa.gov) |
-| Riverside State Park | 509-456-5064 | [Riverside](https://parks.wa.gov) |
-| Crawford State Park (Gardner Cave) | 509-466-4065 | [Crawford](https://parks.wa.gov) |
+| **Idaho Dept. of Parks & Rec.** | [208-769-1511](tel:2087691511) | [idahoparks.org](https://idahoparks.org) |
+| Farragut State Park | [208-683-2425](tel:2086832425) | [Farragut](https://idahoparks.org/parks/farragut) |
+| Heyburn State Park | [208-686-1308](tel:2086861308) | [Heyburn](https://idahoparks.org/parks/heyburn) |
+| Old Mission State Park | [208-682-3814](tel:2086823814) | [Park Link](https://idahoparks.org) |
+| Priest Lake State Park | [208-443-2200](tel:2084432200) | [Priest Lake](https://idahoparks.org/parks/priest-lake) |
+| Round Lake State Park | [208-263-3489](tel:2082633489) | [Round Lake](https://idahoparks.org/parks/round-lake) |
+| **Washington State Parks** | [509-902-8500](tel:5099028500) | [parks.wa.gov](https://parks.wa.gov) |
+| Mount Spokane State Park | [509-456-3964](tel:5094563964) | [Mount Spokane](https://parks.wa.gov) |
+| Riverside State Park | [509-456-5064](tel:5094565064) | [Riverside](https://parks.wa.gov) |
+| Crawford State Park (Gardner Cave) | [509-466-4065](tel:5094664065) | [Crawford](https://parks.wa.gov) |
 | **Bureau of Land Management (BLM)** | | |
-| Coeur d'Alene Field Office | 208-769-5000 | [BLM Idaho](https://www.blm.gov/idaho) |
-| Spokane District Office | 509-536-1200 | [BLM Washington](https://www.blm.gov/washington) |
+| Coeur d'Alene Field Office | [208-769-5000](tel:2087695000) | [BLM Idaho](https://www.blm.gov/idaho) |
+| Spokane District Office | [509-536-1200](tel:5095361200) | [BLM WA](https://www.blm.gov/washington) |
 | **Idaho Dept. of Lands** | | |
-| St. Maries Area | 208-245-8100 | [idl.idaho.gov](https://www.idl.idaho.gov) |
-| Priest River Area | 208-448-2302 | [idl.idaho.gov](https://www.idl.idaho.gov) |
-| Mica Area (Rathdrum) | 208-769-1577 | [idl.idaho.gov](https://www.idl.idaho.gov) |
+| St. Maries Area | [208-245-8100](tel:2082458100) | [idl.idaho.gov](https://www.idl.idaho.gov) |
+| Priest River Area | [208-448-2302](tel:2084482302) | [idl.idaho.gov](https://www.idl.idaho.gov) |
+| Mica Area (Rathdrum) | [208-769-1577](tel:2087691577) | [idl.idaho.gov](https://www.idl.idaho.gov) |
 | **Wildlife Agencies** | | |
-| Idaho Fish & Game (Panhandle) | 208-769-1414 | [idfg.idaho.gov](https://idfg.idaho.gov) |
-| Kootenai NWR | 208-267-3888 | [Kootenai NWR](https://www.fws.gov/refuge/kootenai) |
-| Dishman Hills Conservancy | 509-926-7949 | [dishmanhills.org](https://dishmanhills.org) |
-| Spokane County Parks | 509-477-4730 | [spokanecounty.org](https://www.spokanecounty.org) |
+| Idaho Fish & Game (Panhandle) | [208-769-1414](tel:2087691414) | [idfg.idaho.gov](https://idfg.idaho.gov) |
+| Kootenai NWR | [208-267-3888](tel:2082673888) | [Kootenai NWR](https://www.fws.gov/refuge/kootenai) |
+| Dishman Hills Conservancy | [509-926-7949](tel:5099267949) | [dishmanhills.org](https://dishmanhills.org) |
+| Spokane County Parks | [509-477-4730](tel:5094774730) | [spokanecounty.org](https://www.spokanecounty.org) |
 
 ---
 
@@ -122,45 +122,45 @@ offices directly. **In any active emergency, dial 911.**
 
 ### Idaho Sheriffs
 
-- **Benewah County Sheriff:** 208-245-2555
-- **Bonner County Sheriff:** 208-263-8417
-- **Boundary County Sheriff:** 208-267-3151
-- **Camas County Sheriff:** 208-764-2261
-- **Clearwater County Sheriff:** 208-476-4521
-- **Kootenai County Sheriff:** 208-446-1300
-- **Shoshone County Sheriff:** 208-556-1114
-- **Idaho State Police (Region 1):** 208-209-8730
+- **Benewah County Sheriff:** [208-245-2555](tel:2082452555)
+- **Bonner County Sheriff:** [208-263-8417](tel:2082638417)
+- **Boundary County Sheriff:** [208-267-3151](tel:2082673151)
+- **Camas County Sheriff:** [208-764-2261](tel:2087642261)
+- **Clearwater County Sheriff:** [208-476-4521](tel:2084764521)
+- **Kootenai County Sheriff:** [208-446-1300](tel:2084461300)
+- **Shoshone County Sheriff:** [208-556-1114](tel:2085561114)
+- **Idaho State Police (Region 1):** [208-209-8730](tel:2082098730)
 
 ### Washington Sheriffs
 
-- **Spokane County Sheriff:** 509-477-2240
-- **Adams County Sheriff:** 509-659-1122
-- **Benton County Sheriff:** 509-735-6555
-- **Chelan County Sheriff:** 509-667-6851
-- **Columbia County Sheriff:** 509-382-2518
-- **Douglas County Sheriff:** 509-884-0941
-- **Franklin County Sheriff:** 509-545-3501
-- **Grant County Sheriff:** 509-754-2011
-- **Kittitas County Sheriff:** 509-962-7525
-- **Lincoln County Sheriff:** 509-725-3501
-- **Okanogan County Sheriff:** 509-422-7200
-- **Pend Oreille County Sheriff:** 509-447-1901
-- **Stevens County Sheriff:** 509-684-5296
-- **Walla Walla County Sheriff:** 509-524-5400
-- **Whitman County Sheriff:** 509-397-6266
+- **Spokane County Sheriff:** [509-477-2240](tel:5094772240)
+- **Adams County Sheriff:** [509-659-1122](tel:5096591122)
+- **Benton County Sheriff:** [509-735-6555](tel:5097356555)
+- **Chelan County Sheriff:** [509-667-6851](tel:5096676851)
+- **Columbia County Sheriff:** [509-382-2518](tel:5093822518)
+- **Douglas County Sheriff:** [509-884-0941](tel:5098840941)
+- **Franklin County Sheriff:** [509-545-3501](tel:5095453501)
+- **Grant County Sheriff:** [509-754-2011](tel:5097542011)
+- **Kittitas County Sheriff:** [509-962-7525](tel:5099627525)
+- **Lincoln County Sheriff:** [509-725-3501](tel:5097253501)
+- **Okanogan County Sheriff:** [509-422-7200](tel:5094227200)
+- **Pend Oreille County Sheriff:** [509-447-1901](tel:5094471901)
+- **Stevens County Sheriff:** [509-684-5296](tel:5096845296)
+- **Walla Walla County Sheriff:** [509-524-5400](tel:5095245400)
+- **Whitman County Sheriff:** [509-397-6266](tel:5093976266)
 
 ### Montana Sheriffs
 
-- **Flathead County Sheriff:** 406-758-5610
-- **Gallatin County Sheriff:** 406-582-2100
-- **Jefferson County Sheriff:** 406-225-4075
-- **Lake County Sheriff:** 406-883-7301
-- **Lincoln County Sheriff:** 406-293-4112
-- **Madison County Sheriff:** 406-843-5301
-- **Mineral County Sheriff:** 406-822-3555
-- **Missoula County Sheriff:** 406-258-4810
-- **Park County Sheriff:** 406-222-4172
-- **Sanders County Sheriff:** 406-827-3584
+- **Flathead County Sheriff:** [406-758-5610](tel:4067585610)
+- **Gallatin County Sheriff:** [406-582-2100](tel:4065822100)
+- **Jefferson County Sheriff:** [406-225-4075](tel:4062254075)
+- **Lake County Sheriff:** [406-883-7301](tel:4068837301)
+- **Lincoln County Sheriff:** [406-293-4112](tel:4062934112)
+- **Madison County Sheriff:** [406-843-5301](tel:4068435301)
+- **Mineral County Sheriff:** [406-822-3555](tel:4068223555)
+- **Missoula County Sheriff:** [406-258-4810](tel:4062584810)
+- **Park County Sheriff:** [406-222-4172](tel:4062224172)
+- **Sanders County Sheriff:** [406-827-3584](tel:4068273584)
 
 ---
 
@@ -168,17 +168,17 @@ offices directly. **In any active emergency, dial 911.**
 
 | Information Service | Phone | Website Link |
 | :--- | :--- | :--- |
-| **Idaho Travel & Road Conditions** | 888-432-7623 | [511.idaho.gov](https://511.idaho.gov) |
-| **Washington WSDOT Traffic Alerts** | 800-695-7623 | [wsdot.com](https://wsdot.com/traffic/trafficalerts) |
-| **Montana MDT Travel Conditions** | 800-226-7623 | [mdt.mt.gov](https://mdt.mt.gov) |
+| **Idaho Travel & Road Conditions** | [888-432-7623](tel:8884327623) | [511.idaho.gov](https://511.idaho.gov) |
+| **WSDOT Traffic Alerts** | [800-695-7623](tel:8006957623) | [wsdot.com](https://wsdot.com/traffic/trafficalerts) |
+| **Montana MDT Travel Conditions** | [800-226-7623](tel:8002267623) | [mdt.mt.gov](https://mdt.mt.gov) |
 | **National Weather Service (Spokane)** | — | [forecast.weather.gov](https://forecast.weather.gov) |
-| **Boundary Dam Flow (Pend Oreille)** | 509-466-3073 | [Seattle City Light](https://www.seattle.gov/light/boundary) |
-| **Box Canyon Dam Flow** | 509-442-3232 | [Pend Oreille PUD](https://popud.org) |
-| **Poison Control Center Hotline** | 800-222-1222 | [poison.org](https://www.poison.org) |
+| **Boundary Dam Flow** | [509-466-3073](tel:5094663073) | [Seattle Light](https://www.seattle.gov/light/boundary) |
+| **Box Canyon Dam Flow** | [509-442-3232](tel:5094423232) | [Pend Oreille PUD](https://popud.org) |
+| **Poison Control Center Hotline** | [800-222-1222](tel:8002221222) | [poison.org](https://www.poison.org) |
 | **Suicide & Crisis Lifeline** | Dial 988 | [988lifeline.org](https://988lifeline.org) |
 """
 
 with open("docs/resources/general/managing-agencies.md", "w", encoding="utf-8") as fp:
     fp.write(md_content)
 
-print("Updated managing-agencies.md for complete MD013 compliance")
+print("Updated managing-agencies.md for complete MD013 compliance with tel links")
